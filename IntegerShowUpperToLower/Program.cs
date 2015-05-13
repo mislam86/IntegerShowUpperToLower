620f31369d4cafef0e61e563d70a4e9bf6fafb52
fatema71 <fatema.tee.08@gmail.com>
2015-05-13 17:21:48 +0600

    Adding First Comment

---
+++ IntegerShowUpperToLower/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerShowUpperToLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two Numbers:");
+            int firtNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            if (firtNumber > secondNumber)
+            {
+                int temp = secondNumber;
+                secondNumber = firtNumber;
+                firtNumber = temp;
+            }
+            for (int i = firtNumber; i <= secondNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
